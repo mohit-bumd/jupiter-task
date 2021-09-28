9de27986a8a2edb04dbb62950adda50d6706a961
mohit <mohit.bumd@gmail.com>
2021-09-28 12:27:15 +0530

    Updated App.tsx

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return <div className="App"/ >;
+  return <div className="App" />;
 }
 
 export default App;
