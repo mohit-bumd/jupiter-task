b0e51653b6c1b204b1710c364b20a73ec68c8e38
mohit <mohit.bumd@gmail.com>
2021-09-29 15:36:27 +0530

    Updated Category component

--- src/Components/Category/index.tsx
+++ src/Components/Category/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import { useContext } from "react";
+import { RiFilterOffLine } from "react-icons/ri";
 import { categories } from "../../Constants";
+import { AppContext, ContextType } from "../../Context";
 import styles from "./style.module.css";
+
 const Category = () => {
+  const { selectCategory, selectedCategories, removeFilter } = useContext(
+    AppContext
+  ) as ContextType;
+
   return (
     <section>
       <div className={styles.CategoryBox}>
@@ -11,14 +18,31 @@ const Category = () => {
         <ul className={styles.CategoryBoxList}>
           {categories.map((item) => {
             return (
-              <li className={styles.categoryBoxItem} key={item.id}>
+              <li
+                onClick={() => selectCategory(item.name)}
+                className={
+                  selectedCategories.includes(item.name)
+                    ? styles.categoryBoxItemSelected
+                    : styles.categoryBoxItem
+                }
+                key={item.id}
+              >
                 {item.name}
               </li>
             );
           })}
+          {selectedCategories.length > 0 && (
+            <li
+              onClick={() => removeFilter()}
+              className={styles.categoryBoxItem}
+            >
+              <RiFilterOffLine />
+            </li>
+          )}
         </ul>
       </div>
     </section>
   );
 };
+
 export default Category;
