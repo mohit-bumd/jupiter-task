a072fbd5e87849bdc326e22321825e4c477b2f29
mohit <mohit.bumd@gmail.com>
2021-09-29 15:37:56 +0530

    Updated home page

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,35 +1,20 @@
-import React from "react";
+import { useContext } from "react";
 import Header from "../../Components/Header";
-import Category from "../../Components/Category";
-// import ProductRow from "./components/ProductRow/ProductRow";
-import { BsArrowRight } from "react-icons/bs";
-import { AiOutlineShoppingCart } from "react-icons/ai";
-import styles from "./style.module.css";
-import ProductsContainer from "../../Components/ProductsContainer";
 import Dashboard from "../../Containers/Dashboard";
+import CartItems from "../../Components/CartItems";
+import { AppContext, ContextType } from "../../Context";
+import styles from "./style.module.css";
 
 const Home = () => {
+  const { cartActive } = useContext(AppContext) as ContextType;
+
   return (
-    <>
-      <main className={styles.mainSection}>
-        <Header />
-        <Dashboard />
-        {/* <aside className={styles.sidebar}>
-          <div className={styles.sidebarHeader}>
-            <div className={styles.sidebarItem}>
-              <BsArrowRight className={styles.sidebarIcon} />
-            </div>
-            <div className={styles.sidebarItem}>
-              <h2 className={styles.sidebarText}>your cart</h2>
-            </div>
-            <div className={styles.sidebarItem}>
-              <AiOutlineShoppingCart />
-              <span>cart</span>
-            </div>
-          </div>
-        </aside> */}
-      </main>
-    </>
+    <main className={styles.mainSection}>
+      <Header />
+      <Dashboard />
+      {cartActive && <CartItems />}
+    </main>
   );
 };
+
 export default Home;
