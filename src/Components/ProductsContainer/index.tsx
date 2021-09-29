dda2e35902d43f7b122f3295ca52b9650512d169
mohit <mohit.bumd@gmail.com>
2021-09-29 15:37:10 +0530

    Updated ProductsContainer component

--- src/Components/ProductsContainer/index.tsx
+++ src/Components/ProductsContainer/index.tsx
@@ -1,15 +1,21 @@
-import React from "react";
-import styles from "./style.module.css";
-import { IoIosAddCircleOutline } from "react-icons/io";
-import data from "../../Data/products.json";
+import { useContext } from "react";
 import ProductCard from "../ProductCard";
+import data from "../../Data/products.json";
+import filterData from "../../Utils/filterData";
+import { AppContext, ContextType } from "../../Context";
+import styles from "./style.module.css";
 
 const ProductsContainer = () => {
+  const { selectedCategories } = useContext(AppContext) as ContextType;
+
+  const productData =
+    selectedCategories.length > 0 ? filterData(data, selectedCategories) : data;
+
   return (
     <section className={styles.productRow}>
       <div>
         <div className={styles.row}>
-          {data.map((item) => {
+          {productData.map((item) => {
             return <ProductCard product={item} key={item.productId.value} />;
           })}
         </div>
