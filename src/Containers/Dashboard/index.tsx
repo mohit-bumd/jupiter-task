dbe34bb310995c29bc8dc4dbe0df1d4938c1541a
mohit <mohit.bumd@gmail.com>
2021-09-29 15:37:33 +0530

    Updated Dashboard container

--- src/Containers/Dashboard/index.tsx
+++ src/Containers/Dashboard/index.tsx
@@ -1,16 +1,13 @@
-import React from "react";
 import Category from "../../Components/Category";
 import ProductsContainer from "../../Components/ProductsContainer";
 import styles from "./style.module.css";
 
 const Dashboard = () => {
   return (
-    <>
-      <div className={styles.dashboardContainer}>
-        <Category />
-        <ProductsContainer />
-      </div>
-    </>
+    <div className={styles.dashboardContainer}>
+      <Category />
+      <ProductsContainer />
+    </div>
   );
 };
 export default Dashboard;
